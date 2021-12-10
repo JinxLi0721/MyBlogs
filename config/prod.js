fc6c5ce0f409c1c429c5933b35b23b8f2fe69ecc
LAPTOP-FPJN483A\n3314 <cathy70477@yahoo.com.tw>
2021-12-10 22:22:46 +0800

    fix: add prod key

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,8 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  redisUrl: process.env.REDIS_URL
+  redisUrl: process.env.REDIS_URL,
+  absoluteUrl: process.env.ABSOLUTE_URL,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY
 };
