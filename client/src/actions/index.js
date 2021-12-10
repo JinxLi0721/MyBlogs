cd1593889960b579d3298bf0be9af1ca3f008f5d
LAPTOP-FPJN483A\n3314 <cathy70477@yahoo.com.tw>
2021-12-10 22:18:40 +0800

    fix: no matter upolad img can success

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -14,19 +14,27 @@ export const handleToken = token => async dispatch => {
 };
 
 export const submitBlog = (values, file , history) => async dispatch => {
-  const uploadConfig = await axios.get("/api/upload");
-  await axios.put(uploadConfig.data.url, file,{
-    headers: {
-      "Content-Type": file.type
-    }
-  })
-  
-  const res = await axios.post('/api/blogs', {
-    ...values, imageUrl: uploadConfig.data.key
-  });
-  
-  history.push('/blogs');
-  dispatch({ type: FETCH_BLOG, payload: res.data });
+
+
+  if(file != null){
+    const uploadConfig = await axios.get("/api/upload");
+       await axios.put(uploadConfig.data.url, file,{
+        headers: {
+          "Content-Type": file.type
+        }
+      })
+        const res = await axios.post('/api/blogs', {
+          ...values, imageUrl: uploadConfig.data.key
+        });
+        history.push('/blogs');
+        dispatch({ type: FETCH_BLOG, payload: res.data });
+
+  }else{
+      const res = await axios.post('/api/blogs', values);
+      history.push('/blogs');
+      dispatch({ type: FETCH_BLOG, payload: res.data });
+  }
+
 };
 
 export const fetchBlogs = () => async dispatch => {
